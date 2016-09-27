6ab8048e1671ccd8267a5b330ee02ece71abb942
Zachary Halpern <zhalper3@cs.binghamton.edu>
2016-09-26 23:27:56 -0400

    ass4 work

--- assignments/Assignment_4/point.c
+++ assignments/Assignment_4/point.c
@@ -9,14 +9,14 @@ int contains_loop(Point *p)
 
 int main()
 {
-	struct Point *test1;
-	struct Point *test2;
+	struct point *test1;
+	struct point *test2;
 	
-	*(test2.x) = 1.0;
-	test2->y = 1.5;
-	test2->z = 1.66;
+	test2.x = 1.0;
+	/*test2->y = 1.5;
+	test2->z = 1.66;*/
 
-	//printf("Hi: %f %f %f\n", test2->x, test2->y, test2->z);
+	/*printf("Hi: %f %f %f\n", test2->x, test2->y, test2->z);*/
 
 	
 	/*test1.x = 2.05;
@@ -24,7 +24,7 @@ int main()
 	test1.z = 2.63;
 	test1.next = &test2;*/
 
-	//contains_loop(&test1);
+	/*contains_loop(&test1);*/
 
 	return 0;
 }
--- assignments/Assignment_4/point.h
+++ assignments/Assignment_4/point.h
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct {
+
+
+
+struct Point{
 	float x;
 	float y;
 	float z;
 	struct Point *next;
-} Point = {0,0,0,NULL};
+};
 
 
 int contains_loop(Point *p);
