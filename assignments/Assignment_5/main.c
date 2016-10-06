40ee8d558760ea85e7ae26f08b1d5c42563e2263
Zach  H <zoosorbs@gmail.com>
2016-10-06 13:25:35 -0400

    questions from test

--- assignments/Assignment_5/main.c
+++ assignments/Assignment_5/main.c
@@ -4,8 +4,28 @@
 
 Node *my_reverse(Node *head)
 {
-	
+	Node *new_start = malloc(sizeof(Node));
+	Node *current, *previous, *next;
+	new_start = head;
+
+	while (head->ptr != NULL)
+	{
+		new_start->ptr = malloc(sizeof(Node));
+		head = head_of_node(head->ptr);
+		new_start = head;
+	}
+
+	/* Now to swap the order of the node list */
+	current = new_start->ptr->ptr
+
 	return head;
+}
+
+Node *head_of_node(void *ptr)
+{
+	ptr = &ptr - 0x4;
+	
+	return ptr;
 }
 
 int main()
