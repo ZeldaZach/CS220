d880942fca7f9501adc6f986b54bae6a0b0f99c0
Zach  H <zoosorbs@gmail.com>
2016-11-11 15:12:25 -0500

    assn10 finished

--- assignments/Assignment_10/bt.c
+++ assignments/Assignment_10/bt.c
@@ -4,37 +4,28 @@
 extern unsigned int *getBasePointer();
 extern int main();
 
-/*void foo1();
+/*
+void foo1();
 void foo2();
 void foo3();
 void print_backtrace(int count);
-*/
 
 
-/*void foo1()
+void foo1()
 {
-	int a = 1;
-	char d = 'd';
-	char* w = "Hello\0";
-
 	foo2();
 }
 
 void foo2()
 {
-	int a = 2;
-	float e = 3.14;
-	long x = 305805;
 	foo3();
 }
 
 void foo3()
 {
-	int a = 3;
-	char e = 'e';
-	char f = 'f';
 	print_backtrace(4);
-}*/
+}
+*/
 
 void print_backtrace(int count)
 {
@@ -47,24 +38,24 @@ void print_backtrace(int count)
 	while (count > 0)
 	{
 		ret_addr = (unsigned int) *(curr_ebp + 0x1);
-		printf("Ret: %x\n", ret_addr);
+		printf("%x\n", ret_addr);
 
-		if ((unsigned int)(main) < ret_addr && ret_addr < (unsigned int)(ptr))
-		{
-			printf("MAIN\n");
+		/* Start address of main < current addr on stack < end address of main */
+		if ((unsigned int)&main < ret_addr && ret_addr < (unsigned int)&ptr)
 			return;
-		}
 
 		curr_ebp = (unsigned int *)*(curr_ebp);
 		count--;
 	}
 }
 
-/*int main()
+/*
+int main()
 {
 	printf("%-20s %x\n", "Address of Main", &main);
 	printf("%-20s %x\n", "Address of Foo1", &foo1);
 	printf("%-20s %x\n", "Address of Foo2", &foo2);
 	printf("%-20s %x\n", "Address of Foo3", &foo3);
 	foo1();
-}*/
+}
+*/
